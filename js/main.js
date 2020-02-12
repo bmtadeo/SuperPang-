17668431a7cdfeeecbfdbe71752290c9be3ea8aa
Borja Martinez Tadeo <borja.martinez.tadeo96@gmail.com>
2020-02-12 16:49:00 +0100

    - ¿Qué significan los parámetros del método context.drawImage(p1,p2,p3,p4,p5,p6,p7,p8,p9) ?
    	P1: es la imagen que se va a pintar.
    	P2: sx, posicion 0 del eje x
    	P3: sy, posicion 0 del eje y
    	P4: sw, porción de pixeles que come de la imagen (anchura)
    	P5: sh, porción de pixeles que come de la imagen (altura)
    	P6: dx, es la posicion x dentro del canvas
    	P7: dy, es la posicion y dentro del canvas
    	P8: dw, reescala la imagen (anchura)
    	P9: dh, reescala la imagen (altura)
    
    - ¿Qué tipo de datos acepta el método context.drawImage en su primer parámetro?  
    Es una url (String) que va enlazada a la carpeta que contiene la imagen.
    
    - ¿Qué hace el método define(x,y,z) de SpriteSheet?  
    Define un sprite, en concreto ‘Buster’, dentro  de la image el objeto buster que esta en la posición (0,0) o la que sea.
    
    - ¿Qué hace el método draw(x1,x2,x3,x4) de la clase SpriteSheet?
      Se le pasa por parámetro el nombre de l buffer que se quiere pintar,  el contexto en que se quiere pintar y la posicion x e y del contexto

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
-function loadImage(url){
-    return new Promise( resolve => {
-        const image = new Image();
-        image.addEventListener('load', () =>{
-            resolve(image);
-        })
-        image.src = url;
-    })
-}
-
-
+import SpriteSheet from "./SpriteSheet.js";
+import {loadImage} from "./loaders.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext('2d');
-context.fillRect(0,0,50,50);
 
 loadImage('img/sprites.png')
 .then(image=>{
-    context.drawImage(image,0,0);
+    const sprites = new SpriteSheet(image,32,32);
+    sprites.define('buster',0,0);
+    sprites.draw('buster',context,150,150);
 });
