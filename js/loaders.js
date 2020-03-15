9227d78b1e723b8f5574386a9ade186e8bf58f3e
Borja Martinez Tadeo <borja.martinez.tadeo96@gmail.com>
2020-03-15 15:57:58 +0100

    Commit BusterAnimation

--- js/loaders.js
+++ js/loaders.js
@@ -13,10 +13,10 @@ export function loadImage(url){
 }
 export function loadBuster(image){
     const spriteSheet = new SpriteSheet(image, 32, 32);
-    spriteSheet.define('buster', 0, 0);
+    spriteSheet.define('buster', 3, 0);
     spriteSheet.define('buster-1', 1, 0);
-    spriteSheet.define('buster-2', 2, 0);
-    spriteSheet.define('buster-3', 3, 0);
+    spriteSheet.define('buster-2', 0, 0);
+    spriteSheet.define('buster-3', 2, 0);
     //BusterAnimation
     const pos = new Vec2D(200,300);
     const size = new Vec2D(32,32);
